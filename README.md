dad0bc2a2e5b1270732c6a23d6c4c0a285571cf4
Elise Okeefe <e.m.okeefe@wustl.edu>
2026-07-28 22:31:20 +0100

    intial python code to parse data for hurricane data

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # landfall-tracker
+
+This is a landfall tracker that tracks all landfalls caused by Hurricanes in Florida since 1900. Once hurricanes in Florida that have created landfalls are determined, it outputs the date of the landfall, the name of the Hurricane, and the max wind speed for each event. 
+
+The data are from e NOAA Best Track Data (HURDAT2) 
+
+# debugging 
+
+First I started debugging after I cleaned the data and saved all the variables. I tried printing and checked if everything printed correctly.
+
+Second I debugged after I casted the cordinates as floats. I checked a few different sample cordinates to ensure the negatives worked properly.
+
+Third I debugged after I wrote the florida method. I checked for the length of records and then created a florida counter to see how many were from florida. I got: 
+Total: 55605
+Florida:  2636
+Which was logically in sound with the data I was given 
+
+# coding logic 
+
+I first cleaned the data. I then stored variables based on the format I found on the NOAA website. I then created a record to hold all of these values. Then I created the method to change the cordinates to the float format. Then I created the method to see if the location was in Florida using information from https://map.motivasi.my.id/. 
+
+After the inital data storage, I began to check whether or not the storm created a landfall. I wanted to make sure I did not use the landfall indicator. From https://scienceinsights.org/what-is-a-landfall/, I found that a landfall is defined by when a storm moves over land after being over water. Therefore, in order to check if a landfall occured, I knew I wanted to loop through records to see for each row, if its in_florida value is the same as the previous. If the previous row was False and the current row is True, that meant there was a landfall. During this method, I also had to make sure the two values I was comparing were from the same storm. 
+
+When I ran this method, I got 370 landfalls. This seemed higher than the numbers I was finding online, so I wanted to consider my logic here. I realized I had not checked if the events were actually hurricanes, so I added that to the if statement and got 146, which is close to the numbers I found online (120+). I then filtered out the data before 1900, which gave me 97, and based on my research this is a realistic number 
+
+# Optimization 
+
+At first, I wanted to filter out everything that was not a hurricane before looping. However, in this context, this could remove important data where the previous value was not yet a hurricane but the current value was. This would count as a hurricane landfall, but would not show up with my current logic. Therefore, I kept it there. 
+
+I then needed to decide how to remove values before 01/01/1900. I wanted to still store these, as this would allow this application to be versitle if someone did want to create another method that included these values. Therefore, I wanted to figure out a way to not include these in the landfall method I created for this prompt without dropping them from the record permantly. However, I found that I would have to use time to filter this nonetheless, and since I did not want to not include them in my overall record, it was better to do this in the if statement than remove them from the record. This allows the application to have access to the data avaliable and have different methods created based on need. 
+
+# Assumptions
+
+1. Bounding 
+I created a box that covers ocean waters off of Florida's coast (Atlantic and Gulf Straits) as well as land. So a storm may slightly come out/inside of the box I created, but never actually crossing onto the land. These slight movements count in my method.
+
---
+++ parser.py
@@ -0,0 +1,94 @@
+records = [] #creates empty record to eventually store cleaned data 
+landfalls = [] #creates empty record to eventually store all landfall events  
+
+def simplify_cords(cordinate):
+   direction = cordinate[-1] #checks the last character to see which direction the cordinate is in (N, S, W, E)
+   val = float(cordinate[:-1]) #grabs the number (not the direction) to change it from string to double 
+   if (direction == "S") or (direction == "W"): #makes negative if its south or west 
+      val = -val 
+   return val  #returns number version of cordinate 
+
+def is_florida(lat, long): 
+   #utilizes https://map.motivasi.my.id/ definition of florida's longitude and latitude to check if a hurricane was in florida 
+   if(lat >= 24.5 and lat <= 31.0) and (long <= -79.0 and long >= -87.0):
+      return True
+   else: 
+      return False 
+
+#Open the data in python in read mode 
+with open("hurdat2.txt", "r") as hurricanedata: 
+    current_storm_id = None
+    current_storm_name = None 
+    for i, line in enumerate(hurricanedata): #loop goes through dataset
+       clean = line.strip() #removes outer whietespace 
+       fields = clean.split(",") #provides list of fields 
+       field = fields[0].strip() #grabs the first element to check on below criteria
+    
+       if(field.isdigit() and len(field) == 8): #checks if this line is a header or data. Storm IDs have letters 
+        #grabs data specific variables & stores them 
+        date = fields[0].strip()
+        time = fields[1].strip()
+        s_id = fields[2].strip() #not using since I am avoiding the landfall indicator variable
+        status = fields[3].strip()
+        latitude = fields[4].strip()
+        latitude = simplify_cords(latitude) #calls method above to change cordinates to numbers rather than strings 
+        longitude = fields[5].strip()
+        longitude = simplify_cords(longitude)
+        max_wind = fields[6].strip()
+        in_florida = is_florida(latitude, longitude)
+
+        #fills the previously defined record with clean hurricane data 
+
+        r = {
+           "storm_id": current_storm_id, 
+           "storm_name": current_storm_name, 
+           "date": date, 
+           "status": status,
+           "latitude": latitude, 
+           "longitude": longitude, 
+           "max_wind": max_wind,
+           "in_florida": in_florida
+        }
+        records.append(r)
+
+       else: #grabs header specific variables & stores them 
+         current_storm_id = fields[0].strip()
+         current_storm_name = fields[1].strip()
+
+#using the defintion of a landfall being a storm moving from water to land, check if the previous location is in the water (in_florida = false) and the current location is on land (in_florida = true)
+def is_landfall(): 
+    for i in range(len(records)): 
+            if i == 0: #avoids indexing to -1 
+                continue 
+            cur_storm = records[i]["storm_id"] #grab current storm ID
+            prev_storm = records[i-1]["storm_id"] #grab previous storm ID
+            prev_val = records[i-1]["in_florida"] #grab previous storm's in_florida boolean 
+            cur_val = records[i]["in_florida"] #grab current storm's in_florida boolean 
+
+            #check if the previous value and current value are the same storm. Then check if the previous storm value was not in Florida but the current storm value is. Also checks if the storm was a hurricane and occured since 1900
+            if(cur_storm == prev_storm) and (prev_val == False) and (cur_val == True) and (records[i]["status"] == "HU") and (int(records[i]["date"]) >= 19000101): 
+                #if the above criteria is true, it was a landfall. store that value into the landfall record created at the top
+                l = {
+                    "storm_id": records[i]["storm_id"], 
+                    "storm_name": records[i]["storm_name"],
+                    "date" : records[i]["date"], 
+                    "max_wind" : records[i]["max_wind"]
+                    }
+                landfalls.append(l)
+    #once the entire record has been looped through, return the landfall events 
+    return landfalls 
+
+florida_landfalls = is_landfall()
+print("Number of landfall events:", len(florida_landfalls))
+for l in florida_landfalls[:15]:
+    print(l)
+
+
+        
+
+
+
+
+
+        
+    
